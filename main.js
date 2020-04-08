1189ea52cd2e20d917e023c9e8aefa4faf41bec1
Karan Singh <thekrsingh@gmail.com>
2020-04-08 19:48:47 +0530

    first upload with index page

---
+++ main.js
@@ -0,0 +1,10 @@
+function aboutHome() {
+	document.querySelector("#aboutHome").scrollIntoView({
+		behavior: "smooth",
+	});
+}
+function goToTop() {
+	document.querySelector("#introHome").scrollIntoView({
+		behavior: "smooth",
+	});
+}
